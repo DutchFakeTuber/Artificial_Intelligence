97926a94004c3cc1daa8a35be6f1cc4ae60fc561
Sybrand_van_Loenen <s.van.loenen@st.hanze.nl>
2020-12-05 20:35:21 +0100

    Updated the main page.

--- Django/results_AI/models.py
+++ Django/results_AI/models.py
@@ -1,4 +1,47 @@
 from django.db import models
 
+# In this case one table can be used for all information; dataset and survey data alike.
 class ResultsTable(models.Model):
-    pass
+    question_1 = models.FloatField()
+    question_2 = models.FloatField()
+    question_3 = models.FloatField()
+    question_4 = models.FloatField()
+    question_5 = models.FloatField()
+    question_6 = models.FloatField()
+    question_7 = models.FloatField()
+    question_8 = models.FloatField()
+    question_9 = models.FloatField()
+    question_10 = models.FloatField()
+    question_11 = models.FloatField()
+    question_12 = models.FloatField()
+    # True = Survey, False = Dataset
+    ourSurvey = models.BooleanField()
+
+# In this case two tables are used; one for the dataset and one for the survey data.
+class ResultsDataset(models.Model):
+    question_1 = models.FloatField()
+    question_2 = models.FloatField()
+    question_3 = models.FloatField()
+    question_4 = models.FloatField()
+    question_5 = models.FloatField()
+    question_6 = models.FloatField()
+    question_7 = models.FloatField()
+    question_8 = models.FloatField()
+    question_9 = models.FloatField()
+    question_10 = models.FloatField()
+    question_11 = models.FloatField()
+    question_12 = models.FloatField()
+
+class ResultsSurvey(models.Model):
+    question_1 = models.FloatField()
+    question_2 = models.FloatField()
+    question_3 = models.FloatField()
+    question_4 = models.FloatField()
+    question_5 = models.FloatField()
+    question_6 = models.FloatField()
+    question_7 = models.FloatField()
+    question_8 = models.FloatField()
+    question_9 = models.FloatField()
+    question_10 = models.FloatField()
+    question_11 = models.FloatField()
+    question_12 = models.FloatField()
