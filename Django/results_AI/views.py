1e0796fb0b6b15db6e4bf49914088c2bafe6b374
Sybrand van Loenen <sybrandvanloenen@hotmail.com>
2020-12-08 13:23:33 +0100

    Updated the Results pages

--- Django/results_AI/views.py
+++ Django/results_AI/views.py
@@ -7,9 +7,20 @@ def index(request):
     return render(request, 'results_AI_index.html', {})
 
 def allQuestions(request):
-    questionTextRaw = Questions.objects.all()
+    questionsSurvey = dataFetcher.SurveyFetcher().FetchAllData()
     information = {
-        "dataQuestions": questionTextRaw,
+        "Question_1": questionsSurvey[0],
+        "Question_2": questionsSurvey[1],
+        "Question_3": questionsSurvey[2],
+        "Question_4": questionsSurvey[3],
+        "Question_5": questionsSurvey[4],
+        "Question_6": questionsSurvey[5],
+        "Question_7": questionsSurvey[6],
+        "Question_8": questionsSurvey[7],
+        "Question_9": questionsSurvey[8],
+        "Question_10": questionsSurvey[9],
+        "Question_11": questionsSurvey[10],
+        "Question_12": questionsSurvey[11],
     }
     return render(request, 'results_AI_allQuestions.html', information)
 
